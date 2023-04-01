b257147e416afd1ad9641446e4d2089e077f477e
Erik Voorbraak <erik.voorbraak@sai360.com>
2023-04-01 16:30:24 +0200

    Added startup logging

--- java/src/main/java/org/knvvl/exam/ExamApplication.java
+++ java/src/main/java/org/knvvl/exam/ExamApplication.java
@@ -1,12 +1,35 @@
 package org.knvvl.exam;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ExamApplication
 {
+	private static final Logger LOGGER = LoggerFactory.getLogger(ExamApplication.class);
+
 	public static void main(String[] args) {
+		logStartupInfo(args);
 		SpringApplication.run(ExamApplication.class, args);
+	}
+
+	private static void logStartupInfo(String[] args)
+	{
+		LOGGER.info("ExamApplication starting");
+		LOGGER.info("Command line params: " + String.join(", ", args));
+		logEnv("spring.datasource.url");
+		logEnv("spring.datasource.username");
+		logEnv("spring.datasource.password");
+	}
+
+	private static void logEnv(String key)
+	{
+		String value = System.getenv(key);
+		if (value != null && key.endsWith("password"))
+			value = "******";
+		if (value != null)
+			LOGGER.info(key + " = " + value);
 	}
 }
